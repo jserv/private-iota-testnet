22d7cf2282a749ba7a8cde71c6544339f2f6ea75
Erik Aigner <aigner.erik@gmail.com>
2018-01-07 00:04:05 +0100

    Parallel node launch script

--- launch_iri_nodes.sh
+++ launch_iri_nodes.sh
@@ -2,22 +2,25 @@
 
 set -o xtrace
 
-function make_node() {
-	DIR="node$1"
+function run_node() {
+	DIR="nodes/$1"
 	PORT=$2
 	NEIGHBOR_PORT=$3
 	echo "node: $DIR, port=$PORT, neighbor_port=$NEIGHBOR_PORT"
-	rm -rf $DIR
 	mkdir -p $DIR
 	cp iri-testnet.jar "${DIR}/"
 	pushd $DIR
-	java -jar iri-testnet.jar --testnet -p $PORT -u $PORT -t $PORT -n "udp://127.0.0.1:$3" >> ../out.txt 2>&1 &
+	java -jar iri-testnet.jar --testnet -p $PORT -u $PORT -t $PORT -n "udp://127.0.0.1:$3" >> ../../nodes.log 2>&1 &
 	popd
 }
 
-make_node 0 14800 14801
-make_node 1 14801 14800
+rm -f nodes.log
+rm -rf nodes
+mkdir -p nodes
+
+# run_node 0 14800 14801
+run_node 1 12345 14800
 
 trap 'kill $(jobs -p)' EXIT
 
-tail -f out.txt
+tail -f nodes.log
