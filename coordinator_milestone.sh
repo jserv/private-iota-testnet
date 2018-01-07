1e67e3986c12e71125558a2622e0c4121e33f706
Erik Aigner <aigner.erik@gmail.com>
2018-01-07 13:43:10 +0100

    Fix coordinator IP

--- coordinator_milestone.sh
+++ coordinator_milestone.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar target/iota-testnet-tools-0.1-SNAPSHOT-jar-with-dependencies.jar Coordinator localhost $1
+java -jar target/iota-testnet-tools-0.1-SNAPSHOT-jar-with-dependencies.jar Coordinator 127.0.0.1 $1
