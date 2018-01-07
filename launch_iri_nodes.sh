9412a5169a430342b573b5279780c4ee56f7088b
Erik Aigner <aigner.erik@gmail.com>
2018-01-07 13:43:02 +0100

    Connect first and last nodes as neighbors

--- launch_iri_nodes.sh
+++ launch_iri_nodes.sh
@@ -8,30 +8,35 @@ function run_node() {
 	mkdir -p $DIR
 	cp iri-testnet.jar "${DIR}/node.jar"
 	pushd $DIR
-	java -jar node.jar --debug --testnet -p $2 -u $3 -t $4 -n "$5" >> ../../nodes.log 2>&1 &
+	java -jar node.jar --debug --revalidate --rescan --sync --testnet -p $2 -u $3 -t $4 -n "$5" >> ../../nodes.log 2>&1 &
 	popd
 }
 
 rm -f nodes.log
-rm -rf nodes
-mkdir -p nodes
+
+# rm -rf nodes
+# mkdir -p nodes
 
 function start_nodes() {
 	N=$1
 	for i in $(seq 1 $N); do
 		HTTP=$((15000 + $i * 10))
 		UDP=$(($HTTP + 1))
+		FIRST_UDP=$((15000 + 1 * 10 + 1))
+		LAST_UDP=$((15000 + $N * 10 + 1))
 		TCP=$(($UDP + 1))
 		HOST="udp://127.0.0.1"
 		NB=""
+
 		if [ $i -eq 1 ]; then
-			NB="$HOST:$(($UDP+10))"
+			NB="$HOST:$(($UDP+10)) $HOST:$LAST_UDP"
 		elif [ $i -eq $N ]; then
-			NB="$HOST:$(($UDP-10))"
+			NB="$HOST:$(($UDP-10)) $HOST:$FIRST_UDP"
 		else
 			NB="$HOST:$(($UDP-10)) $HOST:$(($UDP+10))"
 		fi
-		run_node $i $HTTP $UDP $TCP $NB
+
+		run_node $i $HTTP $UDP $TCP "$NB"
 	done
 }
 
