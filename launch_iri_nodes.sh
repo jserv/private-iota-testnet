8fa3d7c2e484b33dba31b839a9f2e39c167a460e
Erik Aigner <aigner.erik@gmail.com>
2018-01-07 13:13:47 +0100

    Automatically launch 5 configured nodes

--- launch_iri_nodes.sh
+++ launch_iri_nodes.sh
@@ -4,11 +4,11 @@
 
 function run_node() {
 	DIR="nodes/$1"
-	echo "node: $DIR, port=$2,udp$3,tcp$4, neighbor_port=$5"
+	echo "node: $DIR, port=$2, udp=$3, tcp=$4, neighbors=$5"
 	mkdir -p $DIR
 	cp iri-testnet.jar "${DIR}/node.jar"
 	pushd $DIR
-	java -jar node.jar --testnet -p $2 -u $3 -t $4 -n "udp://127.0.0.1:$5" >> ../../nodes.log 2>&1 &
+	java -jar node.jar --debug --testnet -p $2 -u $3 -t $4 -n "$5" >> ../../nodes.log 2>&1 &
 	popd
 }
 
@@ -16,7 +16,26 @@ rm -f nodes.log
 rm -rf nodes
 mkdir -p nodes
 
-run_node 0 15000 15001 15002 14600
+function start_nodes() {
+	N=$1
+	for i in $(seq 1 $N); do
+		HTTP=$((15000 + $i * 10))
+		UDP=$(($HTTP + 1))
+		TCP=$(($UDP + 1))
+		HOST="udp://127.0.0.1"
+		NB=""
+		if [ $i -eq 1 ]; then
+			NB="$HOST:$(($UDP+10))"
+		elif [ $i -eq $N ]; then
+			NB="$HOST:$(($UDP-10))"
+		else
+			NB="$HOST:$(($UDP-10)) $HOST:$(($UDP+10))"
+		fi
+		run_node $i $HTTP $UDP $TCP $NB
+	done
+}
+
+start_nodes 5
 
 trap 'kill $(jobs -p)' EXIT
 
